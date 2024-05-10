1bc427e7597aac6beac1adaa76bbdc38dead4515
Laye SALL <sall19@gmail.com>
2024-05-10 04:41:56 +0200

    add filtre

--- routes/led.js
+++ routes/led.js
@@ -4,9 +4,16 @@ const router = require("express").Router();
 
 
 router.get('/', async(req, res)=>{
+    const {led, state} = req.query
+    let response
     try {
-      const leds = await Led.find();
-      res.status(200).json(leds); 
+        if (led || state) {
+            response = led ? await Led.find({ led }): await Led.find({ "state": state })
+        }else{
+            response = await Led.find();
+        }
+       
+      res.status(200).json(response); 
     } catch (error) {
       res.status(500).json({ message: error.message }); 
     }
